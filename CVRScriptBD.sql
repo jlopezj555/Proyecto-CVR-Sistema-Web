592cc9cdea66a812f2dd85a7a6982a44fd7e58a7
jlopezj555 <jlopezj24@miumg.edu.gt>
2025-09-08 08:12:43 -0600

    Scripts Ejemplo modificados

--- CVRScriptBD.sql
+++ CVRScriptBD.sql
@@ -8,8 +8,7 @@ CREATE TABLE Empleado (
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100)NOT NULL,
     correo VARCHAR(120) UNIQUE NOT NULL,
-    contraseña VARCHAR(120) UNIQUE NOT NULL,
-    cargo VARCHAR(100)
+    contraseña VARCHAR(120) NOT NULL
 );
 
 -- Tabla de roles
@@ -22,7 +21,10 @@ CREATE TABLE Rol (
 -- Tabla de empresas
 CREATE TABLE Empresa (
     id_empresa INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_empresa VARCHAR(120) NOT NULL
+    nombre_empresa VARCHAR(120) NOT NULL,
+    direccion_empresa VARCHAR(120)NOT NULL,
+    telefono_empresa VARCHAR(20)NOT NULL,
+    correo_empresa VARCHAR(120) UNIQUE NOT NULL
 );
 
 -- Tabla de cuentas
@@ -54,3 +56,31 @@ CREATE TABLE AsignacionRol (
         ON UPDATE CASCADE,
     UNIQUE (id_empleado, id_rol, id_cuenta) -- evita duplicados
 );
+
+CREATE TABLE EtapaCatalogo (
+    id_etapa INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_etapa VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(200),
+    es_revision BOOLEAN DEFAULT FALSE -- para distinguir revisiones de otras etapas
+);
+
+
+CREATE TABLE EtapaCuenta (
+    id_etapa_cuenta INT AUTO_INCREMENT PRIMARY KEY,
+    id_cuenta INT NOT NULL,
+    id_asignacion INT NOT NULL, -- Empleado+Rol asignado
+    id_etapa INT NOT NULL,
+    estado ENUM('Pendiente','En progreso','Completada','Rechazada') DEFAULT 'Pendiente',
+    motivo_rechazo VARCHAR(300), -- si aplica
+    etapa_origen_error INT,      -- id_etapa donde se detectó el error
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_fin TIMESTAMP NULL,
+    FOREIGN KEY (id_cuenta) REFERENCES Cuenta(id_cuenta)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_asignacion) REFERENCES AsignacionRol(id_asignacion)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_etapa) REFERENCES EtapaCatalogo(id_etapa)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (etapa_origen_error) REFERENCES EtapaCatalogo(id_etapa)
+);
+
---
+++ InsertEjemplo.sql
@@ -0,0 +1,6 @@
+INSERT INTO EtapaCatalogo (nombre_etapa, descripcion, es_revision)
+VALUES ('Recepción', 'Recepción inicial', FALSE),
+       ('Revisión 1', 'Revisión de datos', TRUE),
+       ('Revisión 2', 'Revisión de documentos', TRUE),
+       ('Revisión 3', 'Revisión de cálculos', TRUE),
+       ('Impresión', 'Impresión final', FALSE);
