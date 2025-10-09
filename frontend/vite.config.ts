26dc81c07962c1e57da3be01c0f1fd9b4d2069fb
jlopezj555 <jlopezj24@miumg.edu.gt>
2025-10-09 08:26:55 -0600

    modificando el vite.config.ts para el railway

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  cacheDir: '.vite', // evita que Vite deje cache dentro de node_modules
   server: {
     host: '0.0.0.0',
     port: 5173,
